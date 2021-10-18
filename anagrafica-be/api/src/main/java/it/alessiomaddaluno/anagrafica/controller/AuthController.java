00dc47f01d67d380dd6169a7caf92480e3284500
Alessio Maddaluno <a.maddaluno@almaviva.it>
2021-10-18 17:34:03 +0200

    logs controller backend

--- anagrafica-be/api/src/main/java/it/alessiomaddaluno/anagrafica/controller/AuthController.java
+++ anagrafica-be/api/src/main/java/it/alessiomaddaluno/anagrafica/controller/AuthController.java
@@ -2,6 +2,8 @@ package it.alessiomaddaluno.anagrafica.controller;
 
 import it.alessiomaddaluno.anagrafica.dto.SignInDTO;
 import it.alessiomaddaluno.anagrafica.dto.SignUpDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import it.alessiomaddaluno.anagrafica.service.AuthService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @RestController
 @RequestMapping("auth")
 public class AuthController {
@@ -20,24 +23,32 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @PostMapping("accedi")
     ResponseEntity<TokenResource> login(@RequestBody SignInDTO dto, HttpServletResponse request){
+        logger.info("[login] START - input: CONFIDENTIAL ");
         String token = this.authService.authenticateUser(dto.getUsername(),dto.getPassword());
         request.setHeader(JwtConfiguration.PARAM,token);
+        logger.info("[login] END OK - output: CONFIDENTIAL ");
         return ResponseEntity.ok(new TokenResource(token));
     }
 
     @PostMapping("registrati")
     ResponseEntity<TokenResource> register(@RequestBody SignUpDTO dto, HttpServletResponse request){
+        logger.info("[register] START - input: CONFIDENTIAL ");
         String token = this.authService.registerUser(dto);
         request.setHeader(JwtConfiguration.PARAM,token);
+        logger.info("[register] END OK - output: CONFIDENTIAL ");
         return ResponseEntity.ok(new TokenResource(token));
     }
 
     @GetMapping("me")
     ResponseEntity<UtenteResource> getMe(HttpServletRequest request){
+        logger.info("[getMe] START - input: void ");
         String token = request.getHeader(JwtConfiguration.PARAM).split("Bearer ")[1];
         UtenteResource utenteResource = this.authService.getMe(token);
+        logger.info("[getMe] END OK - output: {} ",utenteResource);
         return ResponseEntity.ok(utenteResource);
     }
 }
--- anagrafica-be/api/src/main/java/it/alessiomaddaluno/anagrafica/controller/PersonaController.java
+++ anagrafica-be/api/src/main/java/it/alessiomaddaluno/anagrafica/controller/PersonaController.java
@@ -2,6 +2,8 @@ package it.alessiomaddaluno.anagrafica.controller;
 
 import it.alessiomaddaluno.anagrafica.dto.PersonaDTO;
 import it.alessiomaddaluno.anagrafica.dto.SearchPersonaDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -16,36 +18,49 @@ public class PersonaController {
     @Autowired
     private PersonaService personaService;
 
+    Logger logger = LoggerFactory.getLogger(PersonaController.class);
+
     // Create
     @PostMapping()
     public ResponseEntity<PersonaResource> insertPersona(@RequestBody PersonaDTO dto){
+        logger.info("[insertPersona] START - input: {} ",dto);
         PersonaResource resource = this.personaService.insert(dto);
+        logger.info("[insertPersona] END OK - output: {} ",resource);
         return ResponseEntity.ok(resource);
     }
 
     // Read
     @PostMapping("search")
     public ResponseEntity<Page<PersonaResource>> search(@RequestBody SearchPersonaDTO dto){
+        logger.info("[search] START - input: {} ",dto);
         Page<PersonaResource> page = this.personaService.search(dto);
+        logger.info("[search] END OK - output: {} ",page.getTotalElements());
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PersonaResource> findById(@PathVariable("id") Long id){
-        return ResponseEntity.ok(this.personaService.findById(id));
+        logger.info("[findById] START - input: {} ",id);
+        PersonaResource resource = this.personaService.findById(id);
+        logger.info("[findById] END OK - output: {} ",resource);
+        return ResponseEntity.ok(resource);
     }
 
     // Delete
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletePersona(@PathVariable("id") Long id){
+        logger.info("[deletePersona] START - input: {} ",id);
         this.personaService.delete(id);
+        logger.info("[deletePersona] END OK - output: void ");
         return ResponseEntity.ok(null);
     }
 
     // Update
     @PutMapping("/{id}")
     public ResponseEntity<PersonaResource> updatePersona(@PathVariable("id") Long id, @RequestBody() PersonaDTO dto){
+        logger.info("[updatePersona] START - input: id - {}, dto - {} ",id,dto);
         PersonaResource resource = this.personaService.update(dto,id);
+        logger.info("[updatePersona] END OK - output: {} ",resource);
         return ResponseEntity.ok(resource);
     }
 
